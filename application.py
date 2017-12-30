7a2014208f23f27efbb01f98cecf2e9ece869168
Marta Jarzębska <martajarz@wp.pl>
2017-12-30 20:13:44 +0100

    20:13

--- application.py
+++ application.py
@@ -32,5 +32,32 @@ def get_db():
 def index():
     return render_template("layout.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in."""
+
+    # # forget any user_id
+    # session.clear()
+
+    # # if user reached route via POST (as by submitting a form via POST)
+    # if request.method == "POST":
+
+    #     # query database for username
+    #     rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+
+    #     # ensure username exists and password is correct
+    #     if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash"]):
+    #         return apology("invalid username and/or password")
+
+    #     # remember which user has logged in
+    #     session["user_id"] = rows[0]["id"]
+
+    #     # redirect user to home page
+    #     return redirect(url_for("index"))
+
+    # # else if user reached route via GET (as by clicking a link or via redirect)
+    # else:
+    return render_template("login.html")
+
 if __name__=="__main__":
     app.run()
