79c2f834412773f0c9db06126746e34d3a08be5f
Marta Jarzębska <martajarz@wp.pl>
2018-02-03 10:35:33 +0100

    cs50 sql

--- application.py
+++ application.py
@@ -31,7 +31,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-db = SQL("sqlite:////home/martajarz/mysite/ShoppingList.db")
+db = SQL("sqlite:///ShoppingList.db")
 
 @app.route("/")
 def index():
