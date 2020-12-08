0c03417b2f9c8db59382e10c31616d014084361f
pio <hesushesus9@gmail.com>
2020-12-08 19:22:39 +0100

    data-cy changes

--- cypress/integration/homepageTests.spec.js
+++ cypress/integration/homepageTests.spec.js
@@ -6,11 +6,11 @@ describe("check if homepage is displayed properly", () => {
     })
 
     it("check name of the homepage", () => {
-        cy.get('.navbar-brand').contains("Shopping list generator");
+        cy.get(Cypress.env("homepageName")).contains("Shopping list generator");
     })
 
     it("check if main functions at top of the page are available", () => {
-        cy.get(':nth-child(1) > .nav-link').contains("Log In");
+        cy.get(Cypress.env("loginButton")).contains("Log In");
         cy.get(Cypress.env("registerButton")).contains("Register");
     })
 })
